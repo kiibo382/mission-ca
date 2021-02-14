d87ae090406390ee8d5876ddded9dc2dacab3245
kiibo <shomok0106k@gmail.com>
2021-02-14 11:13:45 +0900

    fix: controller, service

--- pkg/controller/users.go
+++ pkg/controller/users.go
@@ -4,19 +4,19 @@ import (
     "github.com/gin-gonic/gin"
     "net/http"
     "github.com/kiibo382/mission-ca/pkg/model"
-    // "github.com/kiibo382/mission-ca/pkg/service"
+    "github.com/kiibo382/mission-ca/pkg/service"
     "strconv"
 )
 
 func UsersAdd(c *gin.Context) {
      user := model.Users{}
-     err := c.Bind(&user)
+     err := c.ShouldBindJSON(&user)
      if err != nil{
          c.String(http.StatusBadRequest, "Bad request")
          return
      }
-    userService :=service.userService{}
-    err = userService.SetBook(&user)
+    userService := service.UserService{}
+    err = userService.SetUsers(&user)
     if err != nil{
         c.String(http.StatusInternalServerError, "Server Error")
         return
@@ -27,23 +27,23 @@ func UsersAdd(c *gin.Context) {
 }
 
 func UsersList(c *gin.Context){
-    bookService :=service.userService{}
-    BookLists := bookService.GetBookList()
+    userService := service.UserService{}
+    usersList := userService.GetUsersList()
     c.JSONP(http.StatusOK, gin.H{
         "message": "ok",
-        "data": BookLists,
+        "data": usersList,
     })
 }
 
 func UsersUpdate(c *gin.Context){
-    book := model.Book{}
-    err := c.Bind(&book)
+    user := model.Users{}
+    err := c.Bind(&user)
     if err != nil{
         c.String(http.StatusBadRequest, "Bad request")
         return
     }
-    bookService :=service.userService{}
-    err = bookService.UpdateBook(&book)
+    bookService := service.UserService{}
+    err = bookService.UpdateUsers(&user)
     if err != nil{
         c.String(http.StatusInternalServerError, "Server Error")
         return
@@ -60,8 +60,8 @@ func UsersDelete(c *gin.Context){
         c.String(http.StatusBadRequest, "Bad request")
         return
     }
-    bookService :=service.userService{}
-    err = bookService.DeleteBook(int(intId))
+    userService := service.UserService{}
+    err = userService.DeleteUsers(int(intId))
     if err != nil{
         c.String(http.StatusInternalServerError, "Server Error")
         return
--- pkg/service/users.go
+++ pkg/service/users.go
@@ -4,10 +4,10 @@ import (
 	"github.com/kiibo382/mission-ca/pkg/model"
 )
 
-type userService struct {}
+type UserService struct {}
 
-func (userService) SetBook(book *model.Users) error {
-    _, err := DbEngine.Insert(book)
+func (UserService) SetUsers(user *model.Users) error {
+    _, err := DbEngine.Insert(user)
     if err!= nil{
          return  err
     }
@@ -15,7 +15,7 @@ func (userService) SetBook(book *model.Users) error {
 }
 
 
-func (userService) GetBookList() []model.Users {
+func (UserService) GetUsersList() []model.Users {
     tests := make([]model.Users, 0)
     err := DbEngine.Distinct("id", "title", "content").Limit(10, 0).Find(&tests)
     if err != nil {
@@ -24,17 +24,17 @@ func (userService) GetBookList() []model.Users {
     return tests
 }
 
-func (userService) UpdateUsers(newBook *model.Users) error {
-    _, err := DbEngine.Id(newBook.Id).Update(newBook)
+func (UserService) UpdateUsers(newUser *model.Users) error {
+    _, err := DbEngine.Id(newUser.Id).Update(newUser)
     if err != nil {
         return err
     }
     return nil
 }
 
-func (userService) DeleteBook(id int) error {
-    book := new(model.Users)
-    _, err := DbEngine.Id(id).Delete(book)
+func (UserService) DeleteUsers(id int) error {
+    user := new(model.Users)
+    _, err := DbEngine.Id(id).Delete(user)
     if err != nil{
         return err
     }
